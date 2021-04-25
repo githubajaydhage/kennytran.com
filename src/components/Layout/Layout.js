0b333937f14253a34afcd00d8fc6919057c4374f
Kenny Tran <kenny@kennytran.com>
2021-04-25 18:09:55 +0100

    style: add global styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,34 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
+
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Global } from '@emotion/react';
 
 import './normalize.css';
 import './fonts.css';
+
 const Layout = (props) => {
     return (
         <>
+            <Global
+                styles={(theme) => ({
+                    'blockquote, dd, dl, figure, h1, h2, h3, h4, h5, h6, p, ul, ol': {
+                        marginTop: 0,
+                        marginBottom: 0,
+                    },
+                    'h1, h2, h3, h4, h5, h6': {
+                        fontWeight: `${theme.fontWeights.heading}`,
+                    },
+                    a: {
+                        color: theme.colors.primary,
+                        textDecoration: 'none',
+                        '&:hover': {
+                            textDecoration: 'underline',
+                        },
+                    },
+                })}
+            />
             <div className="layout">{props.children}</div>
         </>
     );
