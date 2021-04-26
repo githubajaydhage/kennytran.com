8f25644cc29b52aa29cb5d57bc9f2a0b183e6201
Kenny Tran <kenny@kennytran.com>
2021-04-26 22:00:17 +0100

    feat: add class to body to detect if JavaScript is enabled

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,18 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
 const React = require('react');
 const Layout = require('./src/components/Layout/Layout').default;
+
+exports.onClientEntry = () => {
+    document.body.className = document.body.className.replace(
+        /\bno-js\b/,
+        'js'
+    );
+};
 
 exports.wrapPageElement = ({ element, props }) => {
     return <Layout {...props}>{element}</Layout>;
