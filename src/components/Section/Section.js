5b375a3135f70be2b96f6e2cf985366a34407367
Kenny Tran <kenny@kennytran.com>
2021-04-27 23:08:53 +0100

    fix: fix scrollbars appearing within sections

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -6,6 +6,8 @@ const Section = (props) => {
         <section
             className={'section ' + props.className}
             sx={{
+                overflow: 'hidden',
+
                 ':first-of-type': {
                     paddingTop: ['160px', '240px'],
                 },
