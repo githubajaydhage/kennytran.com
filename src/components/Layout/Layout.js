2527ec396454a8b4d46e55439681c21601fb14aa
Kenny Tran <kenny@kennytran.com>
2021-05-04 21:49:06 +0100

    style: hide layout before initial client render

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -15,7 +15,7 @@ const Layout = (props) => {
         <>
             <Global
                 styles={(theme) => ({
-                    'body.no-js': {
+                    'body.no-js .layout': {
                         display: 'none'
                     },
                     'blockquote, dd, dl, figure, h1, h2, h3, h4, h5, h6, p, ul, ol': {
